91fa800715da215c11bbe1cc5cc92a1d94fb515d
raindr0-0p <buntykiaunty@gmail.com>
2026-05-23 04:48:02 +0530

    realtime updates, max char in name, basic XSS prevention, edit timestamp

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import {db} from "./firebase.js";
-import {collection,addDoc,getDocs,
+import {collection,addDoc,onSnapshot,
         deleteDoc,updateDoc,doc,
         serverTimestamp,query,orderBy
 } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
@@ -7,47 +7,52 @@ import {collection,addDoc,getDocs,
 const saveBtn=document.getElementById("saveBtn");
 const messagesDiv=document.getElementById("messages");
 
-async function loadMessages(){
-    messagesDiv.innerHTML="";
-    const querySnapshot=await getDocs(query(collection(db,"messages"),orderBy("createdAt","desc")));
-    querySnapshot.forEach((doc)=>{
-        const data=doc.data();
-        messagesDiv.innerHTML+=`
-            <div class="message">
-                <h3>${data.name}:</h3>
-                <p>${data.message}</p>
-                <small>${data.createdAt?data.createdAt.toDate():"No timestamp"}</small>
-                <br><br>
-                <button onclick="deleteMessage('${doc.id}')">Delete</button>
-                <button onclick="editMessage('${doc.id}')">Edit</button>
-            </div>
-        `;
+function loadMessages(){
+    onSnapshot(query(collection(db,"messages"),orderBy("createdAt","desc")),(querySnapshot)=>{
+        messagesDiv.innerHTML="";
+        querySnapshot.forEach((doc)=>{
+            const data=doc.data();
+            messagesDiv.innerHTML+=`
+                <div class="message">
+                    <h3>${data.name}:</h3>
+                    <p>${data.message}</p>
+                    <small>${data.createdAt?data.createdAt.toDate():"No timestamp"}</small>
+                    <br>
+                    <small>${data.editedAt?"Latest edition: "+data.editedAt.toDate():""}</small>
+                    <br><br>
+                    <button onclick="deleteMessage('${doc.id}')">Delete</button>
+                    <button onclick="editMessage('${doc.id}')">Edit</button>
+                </div>
+            `;
+        });
+        document.getElementById("counter").innerHTML=`Total messages: ${querySnapshot.size}`;
     });
-    document.getElementById("counter").innerHTML=`Total messages: ${querySnapshot.size}`;
 }
 
 saveBtn.addEventListener("click",async()=>{
     const name=document.getElementById("name");
+    if(name.value.length>100){
+        alert("Maximum 100 characters for name");
+        return;
+    }
     const message=document.getElementById("message");
     if(name.value===""||message.value===""){
         alert("Fill all fields");
         return;
     }
     await addDoc(collection(db,"messages"),{
-        name:name.value,
-        message:message.value,
+        name:escapeHTML(name.value),
+        message:escapeHTML(message.value),
         createdAt:serverTimestamp()
     })
     name.value="";
     message.value="";
-    loadMessages();
 })
 
 window.deleteMessage=async function(id){
     if(confirm("Delete this message?")){
         await deleteDoc(doc(db,"messages",id));
         alert("Message deleted");
-        loadMessages();
     }
 }
 
@@ -57,9 +62,18 @@ window.editMessage=async function(id){
         return;
     }
     await updateDoc(doc(db,"messages",id),{
-        message:newMessage
+        message:newMessage,
+        editedAt:serverTimestamp()
     });
-    loadMessages();
+}
+
+function escapeHTML(str){
+    return str
+        .replace(/&/g,"&amp;")
+        .replace(/</g,"&lt;")
+        .replace(/>/g,"&gt;")
+        .replace(/"/g,"&quot;")
+        .replace(/'/g,"&#039;");
 }
 
 loadMessages();
